43c79e371d654068f7014a6a4f2ac479b7bbc7e7
VisalakshiPonnada <ponnadavisalakshi@gmail.com>
2026-09-04 09:11:05 +0530

    Create alert.h

---
+++ alert.h
@@ -0,0 +1,14 @@
+#define GREEN_LED_PIN 28
+#define YELLOW_LED_PIN 29
+#define RED_LED_PIN 30
+#define BUZZER_PIN 5
+
+void AlertInit(void);
+void GREENLED_ON(void);
+void GREENLED_OFF(void);
+void YELLOWLED_ON(void);
+void YELLOWLED_OFF(void);
+void REDLED_ON(void);
+void REDLED_OFF(void);
+void BUZZER(void);
+void BUZZER_OFF(void);
