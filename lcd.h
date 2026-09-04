8354ae7909b00ab4a3c7640405fba63350ffcb13
VisalakshiPonnada <ponnadavisalakshi@gmail.com>
2026-09-04 23:38:06 +0530

    Create lcd.h

---
+++ lcd.h
@@ -0,0 +1,10 @@
+#include "types.h"
+void writeLCD(U8);
+void cmdLCD(U8);
+void InitLCD(void);
+void charLCD(U8);
+void strLCD(s8 *);
+void U32LCD(U32);
+void s32LCD(s32);
+void F32LCD(f32,U8);
+void BUILDCGRAM(U8 *,U8);
