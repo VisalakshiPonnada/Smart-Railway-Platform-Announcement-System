22f9d7622b601deecb43554e41769deb9678f621
VisalakshiPonnada <ponnadavisalakshi@gmail.com>
2026-09-04 23:26:16 +0530

    Create edit.h

---
+++ edit.h
@@ -0,0 +1,6 @@
+void Adminconfigmode(void);
+void edittraininfo(void);
+void edittrain(U8 index);
+void editrtc(void);
+void editdelay(U32 index);
+void updatetraintime(U32 index,U32 delayhour,U32 delayminute);
